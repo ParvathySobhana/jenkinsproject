33eb32585e76e8bec3688c8761e523d8b97472d4
rethi <rethi@DESKTOP-1A4HR8H>
2022-08-29 17:47:47 +0530

    Hi

---
+++ src/main/java/scripts/Testcase4.java
@@ -0,0 +1,11 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase4 {
+	@Test
+	public void tc1(){
+		Reporter.log("hello api",true);
+	}
+}
